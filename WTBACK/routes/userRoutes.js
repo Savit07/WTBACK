03b9391db49569d686f88aaa57074bc31bb5eaf9
Savit07 <savitpandita7@gmail.com>
2024-10-23 00:26:42 +0530

    remove the architecture

--- WTBACK/routes/userRoutes.js
+++
@@ -1,8 +0,0 @@
-// routes/userRoutes.js
-const express = require('express');
-const { registerUser, gt } = require('../controllers/userController');
-const router = express.Router();
-
-router.post('/add', registerUser);
-router.get('/',gt);
-module.exports = router;
