96cea1d8aee90e22a20e292507602b49956d3a1e
Savit07 <savitpandita7@gmail.com>
2024-10-22 23:38:23 +0530

    added the new vercel

--- WTBACK/index.js
+++ WTBACK/index.js
@@ -10,7 +10,7 @@ connectDB();
 
 // Middleware
 app.use(express.json());
-app.use('/', userRoutes);
+app.use('/api/users', userRoutes);
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
