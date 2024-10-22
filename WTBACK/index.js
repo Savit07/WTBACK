50969e2defb9798436d4a8babd35cf21c1f55fa6
Savit07 <savitpandita7@gmail.com>
2024-10-22 23:31:09 +0530

    vercel added succesfullky

--- WTBACK/index.js
+++ WTBACK/index.js
@@ -10,7 +10,7 @@ connectDB();
 
 // Middleware
 app.use(express.json());
-app.use('/api/users', userRoutes);
+app.use('/', userRoutes);
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
